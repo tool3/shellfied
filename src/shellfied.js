32751d7950cf0e38617fdc909eeb6142c6ddef8a
Tal Hayut <tal.hayut@applitools.com>
2020-12-25 16:50:31 +0200

    feat: adding /text path

--- src/shellfied.js
+++ src/shellfied.js
@@ -2,17 +2,16 @@ const shellfie = require('shellfie');
 const express = require('express');
 const bodyParser = require("body-parser");
 const cors = require('cors');
-const path = require( 'path');
+const path = require('path');
 const fs = require('fs').promises;
 
 const app = express();
 const port = process.env.PORT || 3000;
-app.use(bodyParser.json());
 app.use(cors());
 app.use('/', express.static(path.join(__dirname, 'greeting')))
 
-app.post('/shellify', async (req, res) => {
-    
+app.post('/shellify', bodyParser.json(), async (req, res) => {
+
     if (!req.body) return res.json({ error: 'must set body' }).status(400);
     if (!req.body.data) return res.json({ error: 'must provide data' }).status(400);
 
@@ -26,19 +25,39 @@ app.post('/shellify', async (req, res) => {
     const config = { ...body, location, name: imgName, ext: extension };
 
     try {
+
         await shellfie(data, config);
         res.sendFile(imgPath);
         await fs.rm(imgPath)
     } catch (error) {
         throw error;
-    }   
+    }
+});
+
+
+app.post('/text', bodyParser.text(), async (req, res) => {
+    if (!req.body) return res.json({ error: 'must set body' }).status(400);
+    const { body } = req;
+
+    const location = path.resolve(__dirname, '../');
+    const imgName = generateId();
+    const imgPath = `${location}/${imgName}.png`;
+    const config = { ...body, location, name: imgName };
+
+    try {
+        await shellfie(body, config);
+        res.sendFile(imgPath);
+        await fs.rm(imgPath)
+    } catch (error) {
+        throw error;
+    }
 });
 
 app.listen(port, () => {
     console.log(`\x1b[32mshellfied server running on \x1b[95m${port}\x1b[0m`);
 });
 
-function generateId () {
+function generateId() {
     const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     const charactersLength = characters.length;
     let result = '';
